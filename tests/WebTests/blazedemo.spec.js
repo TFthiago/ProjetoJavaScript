8bf29e5c9cda8a69119979a397a6ea4edd2ae094
TFthiago <tfreitasto21@gmail.com>
2024-03-29 16:54:02 -0300

    blazedemoP

---
+++ tests/WebTests/blazedemo.spec.js
@@ -0,0 +1,26 @@
+const { Builder , By } = require("selenium-webdriver");
+const { assert } = require("assert");
+
+describe('Blazedemo por programação', () => {
+    let driver;
+    let url = "https://blazedemo.com";
+
+    beforeEach(async function() {
+        driver = await new Builder().forBrowser("chrome").build();
+        await driver.manage().setTimeouts({implicit: 30000});
+    })
+
+    afterEach(async function() {
+        driver.quit();
+    })
+
+    it('Comprar passagem', async() => {
+        await driver.get(url);
+        await driver.findElement(By.css("option[value = 'Portland']")).click();
+        await driver.findElement(By.css("option[value = 'Rome']")).click();
+        await driver.findElement(By.css("[class = 'btn btn-primary']")).click();
+        await expect(driver.getTitle()).resolves.toBe("BlazeDemo - reserve");
+        await expect(driver.findElement(By.css("div h3")).getText()).resolves.toBe("Flights from Portland to Rome:");
+
+    })
+})
