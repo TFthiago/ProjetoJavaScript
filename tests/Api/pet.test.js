fa092f66678e20073404b60b8b56251d65e983b7
TFthiago <tfreitasto21@gmail.com>
2024-06-07 17:15:15 -0300

    alterações/testes de API

--- tests/Api/pet.test.js
+++ tests/Api/pet.test.js
@@ -1,14 +1,13 @@
-//const { expect } = require("chai");
 const supertest = require("supertest");
-//const assert = require("chai").assert;
 import {assert} from "chai";
 
-let petId = 4879531;
-let petName = "Radahn";
-let petStatus = "available";
+//let petId = 4879531;
+let petId;
+let petName;
+let petStatus;
 
-describe('Petstore Swagger - pet', () => {
-    let request = supertest("https://petstore.swagger.io/v2")   
+describe('Petstore Swagger - Teste CRUD - pet', () => {
+    let request = supertest("https://petstore.swagger.io/v2");
 
     it ('POST pet', () => {
 
@@ -17,14 +16,13 @@ describe('Petstore Swagger - pet', () => {
             .post("/pet")
             .send(jsonBody)
             .then((resp) => {
-                expect(resp.statusCode).toBe(200)
-                expect(resp.body.id).toBe(petId)
-                ;
-
-                assert.equal(resp.statusCode, 200)
-                assert.equal(resp.body.id, petId)
-                assert.equal(resp.body.name, petName)
-                assert.equal(resp.body.status, petStatus)
+                petId = resp.body.id;
+                petName = resp.body.name;
+                petStatus = resp.body.status;
+                assert.equal(resp.statusCode, 200);
+                assert.equal(resp.body.id, petId);
+                assert.equal(resp.body.name, petName);
+                assert.equal(resp.body.status, petStatus);
             });
         
     });
@@ -34,37 +32,35 @@ describe('Petstore Swagger - pet', () => {
         return request
             .get("/pet/" + petId)
             .then((resp) => {
-                expect(resp.statusCode).toBe(200)
-                expect(resp.body.id).toBe(petId)
-                expect(resp.body.name).toEqual(petName)
-                expect(resp.body.status).toBe(petStatus)
-                ;
-            })
+                assert.equal(resp.statusCode, 200);
+                assert.equal(resp.body.id, petId);
+                assert.isArray(resp.body.tags, 'tags deve ser um array');
+                assert.equal(resp.body.tags[0].id, 15, 'tags[0].id deve ser 15');
+                assert.equal(resp.body.status, petStatus);
+            });
         
     });
 
     it('PUT pet', () => {
         
-        const jsonBody = require("../../vendors/pet1 PUT.json")
+        const jsonBody = require("../../vendors/pet1 PUT.json");
         return request
             .put("/pet")
             .send(jsonBody)
             .then((resp) => {
-                expect(resp.statusCode).toBe(200)
-                expect(resp.body.status).toBe("sold")
-                ;
-            })
+                assert.equal(resp.statusCode, 200);
+                assert.equal(resp.body.status, "sold");
+            });
     });
 
     it('DELETE pet', () => {
         return request
             .delete("/pet/" + petId)
             .then((resp) => {
-                expect(resp.body.code).toEqual(200)
-                expect(resp.body.type).toBe("unknown")
-                expect(resp.body.message).toBe("" + petId + "")
-                ;
-            })
+                assert.equal(resp.body.code, 200);
+                assert.equal(resp.body.type, "unknown");
+                assert.equal(resp.body.message, "" + petId + "");
+            });
         
     });
-});
+})
--- tests/Api/user.test.js
+++ tests/Api/user.test.js
@@ -1,17 +1,12 @@
-//const { expect } = require("chai")
-//const { expect } = require("chai")
-//const { expect } = require("chai")
-const supertest = require("supertest")
+const supertest = require("supertest");
 
-describe('Petstore Swagger - User', () => {
-    let request = supertest("https://petstore.swagger.io/v2")
+describe('Petstore Swagger - Teste CRUD - User', () => {
+    let request = supertest("https://petstore.swagger.io/v2");
 
-    let userId = 152997
-    let username = "TF"
-    let lastName = "Fergusson"
-    let password = "123987"
-
-    //let userId2 = "152998"
+    let userId = 152997;
+    let username = "TF";
+    let lastName = "Fergusson";
+    let password = "123987";
 
     it('POST user', () => {
         const jsonBody = require("../../vendors/user1.json");
@@ -20,40 +15,37 @@ describe('Petstore Swagger - User', () => {
                 .post("/user")
                 .send(jsonBody)
                 .then((resp) => {
-                    expect(resp.statusCode).toBe(200)
-                    expect(resp.body.code).toBe(200)
-                    expect(resp.body.type).toBe("unknown")
-                    expect(resp.body.message).toEqual("" + userId + "") 
-                    ;
-                })
+                    expect(resp.statusCode).toBe(200);
+                    expect(resp.body.code).toBe(200);
+                    expect(resp.body.type).toBe("unknown");
+                    expect(resp.body.message).toEqual("" + userId + ""); 
+                });
     });
 
     it('GET user', () => {
         return request
             .get("/user/" + username)
             .then((resp) => {
-                expect(resp.statusCode).toBe(200)
-                expect(resp.body.id).toEqual(userId)
-                expect(resp.body.username).toBe(username)
-                expect(resp.body.lastName).toBe(lastName)
-                expect(resp.body.password).toBe(password)
-                ;
-            })
+                expect(resp.statusCode).toBe(200);
+                expect(resp.body.id).toEqual(userId);
+                expect(resp.body.username).toBe(username);
+                expect(resp.body.lastName).toBe(lastName);
+                expect(resp.body.password).toBe(password);
+            });
         
     });
 
     it('PUT user', () => {
-        const jsonBody = require("../../vendors/user1 PUT.json")
+        const jsonBody = require("../../vendors/user1 PUT.json");
         return request
             .put("/user/" + username)
             .send(jsonBody)
             .then((resp) => {
-                expect(resp.statusCode).toBe(200)
-                expect(resp.body.code).toBe(200)
-                expect(resp.body.type).toBe("unknown")
-                expect(resp.body.message).toBe("" + userId + "")
-                ;
-            })
+                expect(resp.statusCode).toBe(200);
+                expect(resp.body.code).toBe(200);
+                expect(resp.body.type).toBe("unknown");
+                expect(resp.body.message).toBe("" + userId + "");
+            });
         
     });
 
@@ -61,12 +53,11 @@ describe('Petstore Swagger - User', () => {
         return request
             .delete("/user/" + username)
             .then((resp) => {
-                expect(resp.statusCode).toEqual(200)
-                expect(resp.body.code).toEqual(200)
-                expect(resp.body.type).toEqual("unknown")
-                expect(resp.body.message).toEqual(username) 
-                ;
-            })
+                expect(resp.statusCode).toEqual(200);
+                expect(resp.body.code).toEqual(200);
+                expect(resp.body.type).toEqual("unknown");
+                expect(resp.body.message).toEqual(username);
+            });
         
     });
 })
